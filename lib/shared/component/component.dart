e45be6810eb706b241f6b54f5434ee655ce6b1b9
mohamedrohiem597 <mohamedrohiem597@gmail.com>
2022-02-08 10:36:38 +0200

    web view screen

--- lib/shared/component/component.dart
+++ lib/shared/component/component.dart
@@ -2,6 +2,7 @@
 import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:todo/modules/web_view/web_view_screen.dart';
 import 'cubit/cubit.dart';
 
 Widget buildTaskItem(Map model,context)=>Dismissible(
@@ -170,53 +171,59 @@ Widget myDivider()=>Padding(
 Widget buildArticleItems(article,context)
 {
   var image =article['urlToImage']?? 'https://www.pulsecarshalton.co.uk/wp-content/uploads/2016/08/jk-placeholder-image.jpg';
-  return Padding(
-    padding: const EdgeInsets.all(18.0),
-    child: Row(
-      children:
-      [
-        Container(
-          width: 120.0,
-          height: 120.0,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10.0),
-            image: DecorationImage(
-              image: NetworkImage(image),
-              fit:BoxFit.cover,
-
-            ),
-          ),
-        ),
-        SizedBox(
-          width: 20.0,
-        ),
-        Expanded(
-          child: Container(
+  return InkWell(
+    onTap: ()
+    {
+      //navigateTo(context, webViewScreen(article['url']),);
+    },
+    child: Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: Row(
+        children:
+        [
+          Container(
+            width: 120.0,
             height: 120.0,
-            child: Column(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              image: DecorationImage(
+                image: NetworkImage(image),
+                fit:BoxFit.cover,
 
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Expanded(child:
-                Text(
-                  '${article['title']}',
-                  style: Theme.of(context).textTheme.bodyText1,
-                  maxLines: 3,
-                  overflow: TextOverflow.ellipsis,
-                )),
-                Text('${article['publishedAt']}',
-                  style: TextStyle(color: Colors.grey,fontWeight: FontWeight.w600),
-
-                ),
-
-
-              ],
+              ),
             ),
           ),
-        ),
-      ],
+          SizedBox(
+            width: 20.0,
+          ),
+          Expanded(
+            child: Container(
+              height: 120.0,
+              child: Column(
+
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Expanded(child:
+                  Text(
+                    '${article['title']}',
+                    style: Theme.of(context).textTheme.bodyText1,
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
+                  )),
+                  Text('${article['publishedAt']}',
+                    style: TextStyle(color: Colors.grey,fontWeight: FontWeight.w600),
+
+                  ),
+
+
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     ),
   );
 }
